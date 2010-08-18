f17c3d81e54a47a1d66c0abbc4be4708db5b3f93
Mike Williams <mdub@dogbiscuit.org>
2010-08-18 22:44:54 +1000

    Use defunkt's "extend self" trick.

--- lib/pith/server.rb
+++ lib/pith/server.rb
@@ -6,26 +6,24 @@ module Pith
 
   module Server
 
-    class << self
-
-      def new(project)
-        Rack::Builder.new do
-          use Rack::CommonLogger, project.logger
-          use Rack::ShowExceptions
-          use Rack::Lint
-          use Pith::Server::AutoBuild, project
-          use Adsf::Rack::IndexFileFinder, :root => project.output_dir
-          run Rack::File.new(project.output_dir)
-        end
+    def new(project)
+      Rack::Builder.new do
+        use Rack::CommonLogger, project.logger
+        use Rack::ShowExceptions
+        use Rack::Lint
+        use Pith::Server::AutoBuild, project
+        use Adsf::Rack::IndexFileFinder, :root => project.output_dir
+        run Rack::File.new(project.output_dir)
       end
-
-      def run(project)
-        app = new(project)
-        Rack::Handler.get("thin").run(app, :Port => 4321)
-      end
-
     end
 
+    def run(project)
+      app = new(project)
+      Rack::Handler.get("thin").run(app, :Port => 4321)
+    end
+
+    extend self
+    
     class AutoBuild
 
       def initialize(app, project)
