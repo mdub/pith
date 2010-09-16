cb82946d06dfdd9ae5549804a69ac8c6ad612c93
Mike Williams <mdub@dogbiscuit.org>
2010-09-17 09:20:41 +1000

    Refactor again, to ease testing.

--- lib/pith/plugins/publication/input.rb
+++ lib/pith/plugins/publication/input.rb
@@ -2,38 +2,49 @@ require "pith/input"
 require "time"
 
 module Pith
+  module Plugins
+    module Publication
+
+      module TemplateMethods
+
+        def published?
+          !published_at.nil?
+        end
+
+        def published_at
+          parse_date(meta["published"])
+        end
+
+        def updated_at
+          parse_date(meta["updated"]) || published_at
+        end
+
+        private
+
+        def parse_date(date_string)
+          Time.parse(date_string) if date_string
+        end
+
+      end
+
+    end
+  end
+end
+
+module Pith
   module Input
 
     class Abstract
-      
+
       def published?
         false
       end
-      
+
     end
 
     class Template
-
-      def published?
-        !published_at.nil?
-      end
-      
-      def published_at
-        parse_date(meta["published"])
-      end
-
-      def updated_at
-        parse_date(meta["updated"]) || published_at
-      end
-
-      private
-
-      def parse_date(date_string)
-        Time.parse(date_string) if date_string
-      end
-
+      include Pith::Plugins::Publication::TemplateMethods
     end
     
   end
 end
-
