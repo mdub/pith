af3092b7ea738df38231bb85ab5785bb69e4a523
Mike Williams <mdub@dogbiscuit.org>
2013-09-23 14:40:14 +1000

    Acknowledge that {published,updated}_at are timestamps, not dates.

--- lib/pith/plugins/publication/input.rb
+++ lib/pith/plugins/publication/input.rb
@@ -12,19 +12,19 @@ module Pith
         end
 
         def published_at
-          parse_date(meta["published"])
+          parse_timestamp(meta["published"])
         end
 
         def updated_at
-          parse_date(meta["updated"]) || published_at
+          parse_timestamp(meta["updated"]) || published_at
         end
 
         private
 
-        def parse_date(date_string)
-          return unless date_string
-          return date_string.to_time if date_string.respond_to?(:to_time)
-          Time.parse(date_string.to_s)
+        def parse_timestamp(arg)
+          return unless arg
+          return arg.to_time if arg.respond_to?(:to_time)
+          Time.parse(arg.to_s)
         end
 
       end
